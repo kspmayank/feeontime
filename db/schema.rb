d9a60d796ce7287a587d06bb3ead352ac1211b5f
Mayank <mayankkashyap95@gmail.com>
2015-12-02 08:41:02 +0530

    Changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202025701) do
+ActiveRecord::Schema.define(version: 20151202030744) do
 
   create_table "institues", force: :cascade do |t|
     t.text     "name"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20151202025701) do
     t.text     "cover"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text     "name"
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "email"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
